4761f168870643385bf87f003199cb031dfd876f
Maria <zaush@bk.ru>
2024-02-18 13:44:53 +0300

    added get all reminders tests

---
+++ internal/bot/service/reminder/get_reminder_test.go
@@ -0,0 +1,67 @@
+package reminder
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	mock_reminder "github.com/Pizhlo/bot-reminder-go-telegram/internal/bot/service/reminder/mocks"
+	"github.com/Pizhlo/bot-reminder-go-telegram/internal/bot/view"
+	"github.com/Pizhlo/bot-reminder-go-telegram/pkg/random"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAll(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userID := int64(1)
+
+	reminderEditor := mock_reminder.NewMockreminderEditor(ctrl)
+	n := New(reminderEditor)
+	n.SaveUser(userID)
+
+	// ожидаемые напоминания, которые возвращает база
+	expectedResult := random.Reminders(5)
+
+	// создаем view для генерации сообщения на основе данных из БД
+	view := view.NewReminder()
+	// подготавливаем сообщение - сохраняем во view
+	view.Message(expectedResult)
+
+	reminderEditor.EXPECT().GetAllByUserID(gomock.Any(), gomock.Any()).Return(expectedResult, nil)
+
+	msg, _, err := n.GetAll(context.Background(), userID)
+	assert.NoError(t, err)
+
+	assert.Equal(t, view.First(), msg)
+}
+
+func TestGetAll_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userID := int64(1)
+
+	reminderEditor := mock_reminder.NewMockreminderEditor(ctrl)
+	n := New(reminderEditor)
+	n.SaveUser(userID)
+
+	sqlErr := sql.ErrNoRows
+
+	// ожидаемые напоминания, которые возвращает база
+	expectedResult := random.Reminders(5)
+
+	// создаем view для генерации сообщения на основе данных из БД
+	view := view.NewReminder()
+	// подготавливаем сообщение - сохраняем во view
+	view.Message(expectedResult)
+
+	reminderEditor.EXPECT().GetAllByUserID(gomock.Any(), gomock.Any()).Return(nil, sqlErr)
+
+	msg, _, err := n.GetAll(context.Background(), userID)
+	assert.EqualError(t, err, sqlErr.Error())
+
+	assert.Equal(t, "", msg)
+}
---
+++ pkg/random/reminders.go
@@ -0,0 +1,37 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/Pizhlo/bot-reminder-go-telegram/internal/bot/model"
+)
+
+// Reminders генерирует указанное количество напоминаний
+func Reminders(n int) []model.Reminder {
+	var reminders []model.Reminder
+
+	for i := 0; i < n; i++ {
+		r := Reminder()
+		reminders = append(reminders, r)
+	}
+
+	return reminders
+}
+
+// Reminder генерирует одно напоминание, заполненное рандомными данными
+func Reminder() model.Reminder {
+	reminderTypes := []model.ReminderType{model.SeveralTimesDayType, model.EverydayType, model.EveryWeekType, model.SeveralDaysType,
+		model.SeveralDaysType, model.OnceMonthType, model.OnceYearType, model.DateType}
+
+	randomType := reminderTypes[rand.Intn(len(reminderTypes))]
+
+	return model.Reminder{
+		ID:      int64(randomInt(0, 10)),
+		TgID:    1,
+		Name:    String(10),
+		Date:    String(10),
+		Type:    randomType,
+		Created: time.Now(),
+	}
+}
