0a783ca8338656fc426f0e5c34374104c111a2d5
Maria <zaush@bk.ru>
2024-04-16 17:57:54 +0300

    updated random reminders to fix errors in tests

--- pkg/random/reminders.go
+++ pkg/random/reminders.go
@@ -22,25 +22,17 @@ func Reminders(n int) []model.Reminder {
 
 // Reminder генерирует одно напоминание, заполненное рандомными данными
 func Reminder() model.Reminder {
-	reminderTypes := []model.ReminderType{model.EverydayType, model.EveryWeekType}
 	weekDays := []string{"sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"}
-
-	randomType := reminderTypes[rand.Intn(len(reminderTypes))]
 
 	r := model.Reminder{
 		ID:      uuid.New(),
 		ViewID:  int64(Int(1, 10)),
 		TgID:    1,
 		Name:    String(10),
-		Date:    String(10),
-		Type:    randomType,
+		Date:    weekDays[rand.Intn(len(weekDays))],
+		Type:    model.EveryWeekType,
 		Time:    "10:10",
 		Created: time.Now(),
-	}
-
-	if r.Type == model.EveryWeekType {
-		randomWd := weekDays[rand.Intn(len(weekDays))]
-		r.Date = randomWd
 	}
 
 	return r
