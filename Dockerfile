ed50cbfa9b1b08cfa91f0ac4dd0945db6f901b04
Maria <zaush@bk.ru>
2024-05-04 21:47:29 +0300

    added dockerfile, migrate.sh, etc

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:alpine AS builder
+WORKDIR /app
+ADD go.mod .
+COPY . .
+RUN go build -o bot .
+
+FROM docker:dind
+WORKDIR /app
+COPY --from=builder /app/bot /app/bot
+COPY migration migration
+COPY migrate.sh .
+RUN chmod +x migrate.sh
+CMD ["./bot"]
+
---
+++ migrate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+set -o allexport
+source .env set
+set +o allexport
+
+docker run --rm -v "./migration" --network bot-reminder_bot-reminder migrate/migrate -source=file:///migration -database "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?sslmode=disable"  $@
